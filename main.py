4b2ba285fe9a6869cfdebb91e60dc555cfd96786
wood <amper.sil.tok@gmail.com>
2022-02-19 18:05:36 +0300

    added headers, arguments, coding login pass for data response

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import threading
 import enum
+import base64
 
 from http_ws_api import BaseHttpApi
 from cpu_monitor import cpu_load
@@ -19,13 +20,18 @@ class DataThread(BaseHttpApi):
         self.data_type = data_type
         self.interval = interval
         self.cpu = cpu
-        self.registration = self.post('/client', data={'login': login, 'pass': password})
+        self.login = login
+        self.password = password
+        self.registration = self.post('/client', json={'login': login, 'pass': password})
         if self.registration.json().get('Error'):
             self.thread_status = ThreadStatus.THREAD_OFF
 
     def _data_thread(self):
         while self.thread_status == ThreadStatus.THREAD_ON:
-            self.post(f'/client/{self.client_id()}', data={'cpu_load': cpu_load(self.interval)})
+            response = self.post(f'/client/{self.client_id()}', data={'cpu_load': cpu_load(self.interval)},
+                                 headers=self.header())
+            if response.status_code != 202:
+                self.thread_status = ThreadStatus.THREAD_OFF
 
     def close_thread(self):
         self.thread_status = ThreadStatus.THREAD_OFF
@@ -37,7 +43,19 @@ class DataThread(BaseHttpApi):
     def client_id(self):
         return self.registration.json().get('client_id')
 
+    def header(self):
+        coding_login_pass = base64.b64encode(f'{self.login}:{self.password}'.encode())
+        return {
+            'Authorization': f'Basic {coding_login_pass.decode()}'
+        }
+
 
 if __name__ == '__main__':
-    cpu = DataThread(data_type=DataType.Megabyte, interval=1, cpu=True, login='wood', password='121222')
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', type=str, help='username')
+    parser.add_argument('-p', '--password', type=str, help='username')
+    parser.add_argument('-i', '--interval', type=int, help='reception data interval', default=1)
+    args = parser.parse_args()
+    cpu = DataThread(data_type=DataType.Megabyte, interval=args.interval, cpu=True, login=args.user, password=args.password)
     cpu.thread_start()
